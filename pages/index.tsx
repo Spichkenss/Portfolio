59708e11f0671e537660060a08dc5eb3b3aa8d3d
Alexander Blinov <spichkens.2002@mail.ru>
2022-06-13 16:25:20 +0300

    Disabled onclick stopping animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,16 +2,25 @@ import type { NextPage } from 'next'
 import styles from '../styles/Home.module.css'
 import Lottie from 'react-lottie'
 import loader from '../loader.json'
+import react from '../react.json'
 
 const Home: NextPage = () => {
 	const defaultOptions = {
 		loop: true,
 		autoplay: true,
 		animationData: loader,
+		rendererSettings: {
+			preserveAspectRatio: 'xMidYMid slice',
+		},
 	}
 	return (
 		<div className={styles.container}>
-			<Lottie options={defaultOptions} height={400} width={400} />
+			<Lottie
+				options={defaultOptions}
+				height={400}
+				width={400}
+				isClickToPauseDisabled={true}
+			/>
 			<h1 className={styles.text}>Coming soon</h1>
 		</div>
 	)
