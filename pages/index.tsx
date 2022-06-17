367d083da28216f7e270887a1648863c94996a53
Alexander Blinov <spichkens.2002@mail.ru>
2022-06-17 12:17:39 +0300

    added theme toggler

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
 import Lottie from 'react-lottie'
 import { useTheme } from '../hooks/useTheme'
 
@@ -17,10 +18,13 @@ const Home: NextPage = () => {
 	const { theme, toggleThemeHandler } = useTheme()
 
 	return (
-		<div
-			className='flex flex-col justify-center items-center min-h-screen dark:bg-black'
-			onClick={() => toggleThemeHandler(theme)}
-		>
+		<div className='flex flex-col justify-center items-center min-h-screen dark:bg-black'>
+			<button
+				onClick={() => toggleThemeHandler(theme)}
+				className='bg-black text-white dark:bg-white dark:text-black p-2 rounded-xl'
+			>
+				{theme === 'dark' ? 'Dark' : 'Light'}
+			</button>
 			<div className='pointer-events-none'>
 				<Lottie
 					options={defaultOptions}
@@ -29,7 +33,7 @@ const Home: NextPage = () => {
 					isClickToPauseDisabled={true}
 				/>
 			</div>
-			<h1 className='text-3xl text-black dark:text-white select-none '>
+			<h1 className='text-3xl text-black dark:text-white select-none'>
 				Coming soon
 			</h1>
 		</div>
